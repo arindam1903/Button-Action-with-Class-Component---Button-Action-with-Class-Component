c48bcc4b23ca3b0bf7b660b36a731fb70dd9d4bd
Arindam Dutta <66781582+arindam1903@users.noreply.github.com>
2021-03-12 01:03:56 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
-import React, {Component, useState} from "react";
+import React, {Component} from "react";
 import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state={display:false};
 	};
+handleClick(){
+ this.setState({display:true});
+ 
+}
 
     render() {
     	return(
     		<div id="main">
+				<button id='click' onClick={this.handleClick} >Click</button>
+{this.state.display===true?<p id='para'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:null}
 				{ /* Do not remove this main div!! */ }
     		</div>
     	);
